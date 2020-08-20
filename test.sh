e81417497c4fe672a72786abdca5bee2ec721332
Nathan Peck <peckn@amazon.com>
2020-08-20 19:07:34 +0000

    Simplifying the tests some more

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm install --prefix test
-APP_URL='https://reverse.test.std.string.services' npm test --prefix test
--- test/reverse-string.test.js
+++ test/reverse-string.test.js
@@ -7,8 +7,6 @@ if (!url) {
   throw new Error('Test process requires that env variable `APP_URL` is set');
 }
 
-console.log(`Running tests against environment ${url}`);
-
 test('should be able to reverse a simple string', async () => {
   const res = await superagent.post(url).send('Hello');
   expect(res.text).toEqual('olleH');
