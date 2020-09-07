f22572dbabbcd9af432c30649895be6eeaf4af6a
Nathan Peck <peckn@amazon.com>
2020-09-07 18:28:42 +0000

    Resetting to initial commit

--- app/index.js
+++ app/index.js
@@ -1,13 +1,11 @@
 var getRawBody = require("raw-body");
-var runes = require('runes');
 var http = require("http");
 
 var server = http.createServer(function (req, res) {
   getRawBody(req)
     .then(function (buf) {
       res.statusCode = 200;
-      let stringRunes = runes(buf.toString());
-      res.end(stringRunes.reverse().join(""));
+      res.end(buf.toString().split("").reverse().join(""));
     })
     .catch(function (err) {
       res.statusCode = 500;
@@ -16,7 +14,3 @@ var server = http.createServer(function (req, res) {
 });
 
 server.listen(3000);
-
-process.once('SIGTERM', function () {
-  server.close();
-});
